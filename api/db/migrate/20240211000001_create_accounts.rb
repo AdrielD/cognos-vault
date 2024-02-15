46f1c9f97df240888eb39639ff44c6abe2565c2b
Adriel Dinelli <adriel.dinelli@gmail.com>
2024-02-15 00:59:39 -0300

    Remove devise

--- api/db/migrate/20240211000001_create_accounts.rb
+++ api/db/migrate/20240211000001_create_accounts.rb
@@ -1,8 +1,9 @@
 class CreateAccounts < ActiveRecord::Migration[7.1]
   def change
     create_table :accounts do |t|
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
+      t.datetime :confirmed_at
     end
   end
 end
